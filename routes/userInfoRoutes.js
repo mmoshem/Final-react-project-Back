511b9f48a2cd0f4b468a1146a03f24422c09c44d
Polina <poinkash28@gmail.com>
2025-06-30 15:51:10 +0300

    new route in userINFOroutes

--- routes/userInfoRoutes.js
+++ routes/userInfoRoutes.js
@@ -2,5 +2,6 @@ import express from 'express';
 import { updateUserInfo } from '../controllers/userInfoController.js';
 
 const router = express.Router();
+router.put('/api/userinfo/:userId', updateUserInfo);
 router.post('/api/userinfo/update', updateUserInfo);
 export default router;
