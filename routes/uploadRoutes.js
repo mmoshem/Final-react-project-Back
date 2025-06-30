cbe4862ac25270b9293c3e178756d25fad6daead
moshe <45424025+mmoshem@users.noreply.github.com>
2025-06-30 12:46:18 +0300

    /

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -3,9 +3,6 @@ import uploadController from '../controllers/uploadController.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Upload route working!' });
-});
 
 router.post('/api/upload', uploadController.uploadMiddleware, uploadController.uploadController);
 
