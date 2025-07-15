d9e9c56fb4eed5c99c3db698ef514f648e62afe2
Polina <poinkash28@gmail.com>
2025-07-16 00:26:22 +0300

    messages kimat done

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,4 +1,5 @@
 import Message from '../models/Message.js';
+import UserInfo from '../models/UserInfo.js';
 
 // Get all messages between two users
 export const getMessagesBetweenUsers = async (req, res) => {
@@ -18,4 +19,24 @@ export const createMessage = async (req, res) => {
   const message = new Message({ from, to, text, time });
   await message.save();
   res.json(message);
+};
+
+// Get all users that the user has conversations with
+export const getConversations = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const messages = await Message.find({
+      $or: [ { from: userId }, { to: userId } ]
+    });
+    const userIds = new Set();
+    messages.forEach(msg => {
+      if (msg.from.toString() !== userId) userIds.add(msg.from.toString());
+      if (msg.to.toString() !== userId) userIds.add(msg.to.toString());
+    });
+    const users = await UserInfo.find({ userId: { $in: Array.from(userIds) } })
+      .select('first_name last_name profilePicture userId');
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch conversations' });
+  }
 }; 
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getMessagesBetweenUsers, createMessage } from '../controllers/messageController.js';
+import { getMessagesBetweenUsers, createMessage, getConversations } from '../controllers/messageController.js';
 
 const router = express.Router();
 
+router.get('/conversations/:userId', getConversations);
 router.get('/:user1/:user2', getMessagesBetweenUsers);
 router.post('/', createMessage);
 
